b178b053d5c8abfdeb9116dfe79507f1f9f65565
Gabriel Michelmann <108688484+gabrielmchlmnn@users.noreply.github.com>
2025-02-28 19:11:26 -0300

    Update views.py

--- sistemaVagas/vaga/views.py
+++ sistemaVagas/vaga/views.py
@@ -9,7 +9,7 @@ from sistemaVagas.functions import aplicaFiltrosVaga, filtrosVaga
 from sistemaVagas.decorators import login_required,admin_required
 from django.http import QueryDict
 
-# teste
+# teste 2
 # Exibe a lista de vagas
 def listar_vagas(request):
     sessao_ativa = 'participante_id' in request.session
